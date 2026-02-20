d96d14a38fba41c39e7601d02daa517e9fd40792
Beqston <bako.rap@gmail.com>
2026-02-20 21:09:47 +0400

    update profile page, add form and styles in frontend

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,24 +4,40 @@ async function getProfile() {
     try{
         profileCNT.innerHTML = "";
         const resMe = await fetch("/api/v1/me");
+
         if(!resMe.ok){
             return alert("User Not Fount");
         }
+
         const userId = await resMe.json();
         const resUsers = await fetch("/api/v1/users");
+
         if(!resUsers.ok){
             return alert("User Not Found!")
         }
+
         const users = await resUsers.json();
-        
         const user = users.data.find((user)=> user._id == userId);
-        console.log(user)
-
-        
-
+        // profile details main container
         profileCNT.innerHTML= `
-            <h2>${user.username}</h2>
-            <h2>${user.email}</h2>
+            <div id="profile-details-cnt">
+
+                <div class="username-email">
+                    <div class="profile-img">${user.username[0].toUpperCase()}</div>
+                    <div>
+                        <h2>${user.username}</h2>
+                        <h2>${user.email}</h2>
+                    </div>
+                </div>
+            
+                <form action="/">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password">
+                    <div><button>Update Password</button></div>
+                </form>
+               
+            </div>
+
         `
 
     }catch(err){
